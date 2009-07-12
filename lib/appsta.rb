2ebb3d22ccb3f28fe0ff00dc991c599932f9f7a8
El Draper <el@ejdraper.com>
2009-07-13 00:09:03 +0100

    moved to version 1.0

--- lib/appsta.rb
+++ lib/appsta.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), "appsta", "github")
 require File.join(File.dirname(__FILE__), "appsta", "git")
 
 module Appsta
-  VERSION = "0.0.1"
+  VERSION = "1.0.0"
   
   include Appsta::Heroku
   include Appsta::GitHub
