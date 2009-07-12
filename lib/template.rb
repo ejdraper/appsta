6efc2dc05d6aecb9cee6a3960c529b1833663e4e
El Draper <el@ejdraper.com>
2009-07-12 01:30:39 +0100

    updated the template to include Appsta, setup the app environments on Heroku, and setup the project repo on GitHub, as well as pushing the code to both Heroku and GitHub

--- lib/template.rb
+++ lib/template.rb
@@ -1,5 +1,6 @@
 gem "appsta"
 require "appsta"
+self.class.instance_eval { include Appsta }
 
 # Remove certain files
 ["README", "public/index.html", "public/favicon.ico", "public/javascripts/*"].each do |path|
@@ -8,3 +9,17 @@ end
 
 # Grab jQuery for use in the app
 run "curl -s -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js"
+
+# Setup the Git repo
+git :init
+git :add => "."
+git :commit => "-a -m 'initial commit by Appsta'"
+
+# Setup the app on Heroku
+[:production, :staging].each { |env| heroku(env) }
+
+# Setup the project repo on GitHub
+github
+
+# Push the app to production, staging, and GitHub
+[:origin, :production, :staging].each { |remote| git :push => "#{remote} master" }
